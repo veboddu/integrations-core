f32883095126e3eaec74b5e8a027a4d13403740a
Justin Iso <justin.james.iso@gmail.com>
2021-01-04 12:28:05 -0500

    Add replication tags for AWS aurora

--- mysql/datadog_checks/mysql/queries.py
+++ mysql/datadog_checks/mysql/queries.py
@@ -31,3 +31,10 @@ SQL_INNODB_ENGINES = """\
 SELECT engine
 FROM information_schema.ENGINES
 WHERE engine='InnoDB' and support != 'no' and support != 'disabled'"""
+
+SQL_REPLICATION_ROLE_AWS_AURORA = """\
+SET sql_notes = 0;  -- Show no warnings if not running on AWS Aurora
+SELECT IF(session_id = 'MASTER_SESSION_ID','writer', 'reader') AS replication_role
+FROM information_schema.replica_host_status
+WHERE server_id = @@aurora_server_id;
+"""
