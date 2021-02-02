5d3bb784107b262d31b160c99ba24908324befa4
Justin Iso <justin.james.iso@gmail.com>
2021-02-02 11:41:57 -0500

    Explicitly return False on mysql when is_aurora cannot be determined

--- mysql/datadog_checks/mysql/mysql.py
+++ mysql/datadog_checks/mysql/mysql.py
@@ -534,6 +534,7 @@ class MySql(AgentCheck):
                 "information may be unavailable: %s",
                 traceback.format_exc(),
             )
+            return False
 
         return self._is_aurora
 
